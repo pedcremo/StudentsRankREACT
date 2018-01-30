7920f4685ffcec3f243a8ae8dc2c1c7b6553c8f3
nicklaus_ <pedcremo@gmail.com>
2018-01-30 17:02:35 +0100

    arrffff

--- src/client/components/gradedTaskPage.js
+++ src/client/components/gradedTaskPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class GradedTaskPage extends React.Component {
     constructor(props){
-        super(props);
-        const terms = props.terms;
-        const Termitems = terms.map((term) =>
-            <option selected value={term.name}>{term.name}</option>            
-        );     
+        super(props);        
+        this.handleSubmit = this.handleSubmit.bind(this);       
+    }
+    
+    handleSubmit(event) {
+        alert('A graded task was submitted');
+        event.preventDefault();
     }
 
     render() {
@@ -14,7 +16,7 @@ class GradedTaskPage extends React.Component {
             <div>
             <h3>Add new Graded Task REACT COMPONENT</h3>  
             We understand as a graded task any test or practice that will be marked by teacher and should be reflected in the final mark.
-            <form id="newGradedTask">
+            <form id="newGradedTask" onSubmit={this.handleSubmit}>
                 <div class="form-group">   
                     <label for="name">Task name:</label>
                     <input type="text" class="form-control" id="idTaskName" name="taskname" value={ this.props.gtInstance.name }  required /> 
@@ -22,12 +24,14 @@ class GradedTaskPage extends React.Component {
                 
                 <div class="form-group">   
                     <label for="description">Task description:</label>
-                    <textarea rows="4" cols="50" class="form-control" id="idTaskDescription" name="taskdescription" value="">{this.props.gtInstance.description}</textarea>
+                    <textarea rows="4" cols="50" class="form-control" id="idTaskDescription" name="taskdescription" value={this.props.gtInstance.description} />
                 </div>
                 <div class="form-group">   
                     <label for="term">Task term:</label>
-                    <select id="termTask">
-                        <Termitems />
+                    <select id="termTask" value={this.props.gtInstance.term}>
+                        {this.props.terms.map((term, index) => (                      
+                            <option key={index} value={term.name}>{term.name}</option>
+                        ))}                    
                     </select>
                     </div>
 
