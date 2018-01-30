4664c5e977ed7c2df40189ac249ace8f0b1fe01e
nicklaus_ <pedcremo@gmail.com>
2018-01-30 17:16:03 +0100

    arrffff

--- src/client/components/gradedTaskPage.js
+++ src/client/components/gradedTaskPage.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 class GradedTaskPage extends React.Component {
     constructor(props){
-        super(props);        
+        super(props);  
+        
+        this.state = {
+            name: props.gtInstance.name,
+            description: props.gtInstance.description,
+            weight: props.gtInstance.weight,
+            term: props.gtInstance.term
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);   
         this.handleSubmit = this.handleSubmit.bind(this);       
     }
     
     handleSubmit(event) {
-        alert('A graded task was submitted');
+        alert('A graded task was submitted '+ JSON.stringify(this.state));
         event.preventDefault();
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
     }
 
     render() {
@@ -17,29 +35,29 @@ class GradedTaskPage extends React.Component {
             <h3>Add new Graded Task REACT COMPONENT</h3>  
             We understand as a graded task any test or practice that will be marked by teacher and should be reflected in the final mark.
             <form id="newGradedTask" onSubmit={this.handleSubmit}>
-                <div class="form-group">   
+                <div className="form-group">   
                     <label for="name">Task name:</label>
-                    <input type="text" class="form-control" id="idTaskName" name="taskname" value={ this.props.gtInstance.name }  required /> 
+                    <input type="text" class="form-control" id="idTaskName" name="taskname" value={ this.state.name }  required /> 
                 </div>
                 
-                <div class="form-group">   
-                    <label for="description">Task description:</label>
-                    <textarea rows="4" cols="50" class="form-control" id="idTaskDescription" name="taskdescription" value={this.props.gtInstance.description} />
+                <div className="form-group">   
+                    <label htmlFor="description">Task description:</label>
+                    <textarea rows="4" cols="50" className="form-control" id="idTaskDescription" name="taskdescription" value={this.state.description} />
                 </div>
-                <div class="form-group">   
-                    <label for="term">Task term:</label>
-                    <select id="termTask" value={this.props.gtInstance.term}>
+                <div className="form-group">   
+                    <label htmlFor="term">Task term:</label>
+                    <select id="termTask" value={this.state.term}>
                         {this.props.terms.map((term, index) => (                      
                             <option key={index} value={term.name}>{term.name}</option>
                         ))}                    
                     </select>
                     </div>
 
-                <div class="form-group">   
-                    <label id="labelWeight" for="weight">Task Weight (0-100 %):</label>
-                    <input type="number" class="form-control" min="1" max ="100" id="idTaskWeight" name="taskweight" value={this.props.gtInstance.weight} required />
+                <div className="form-group">   
+                    <label id="labelWeight" htmlFor="weight">Task Weight (0-100 %):</label>
+                    <input type="number" className="form-control" min="1" max ="100" id="idTaskWeight" name="taskweight" value={this.state.weight} required />
                 </div>
-                    <input type="submit" class="btn btn-primary" value="Save" />
+                    <input type="submit" className="btn btn-primary" value="Save" />
                 </form> 
             </div>             
         );
