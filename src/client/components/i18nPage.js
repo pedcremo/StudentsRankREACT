09b3950e982c23ec234c7ad6c0766a7cf338953b
gtormo <gonzalotormobas@gmail.com>
2018-02-26 21:13:31 +0100

    Some i18n-react examples added

--- src/client/components/i18nPage.js
+++ src/client/components/i18nPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import T from 'i18n-react';
 
 class I18nPage extends React.Component {
     constructor(props){
@@ -6,7 +7,11 @@ class I18nPage extends React.Component {
         this.state = {                
         };        
         //this.handleInputChange = this.handleInputChange.bind(this);   
-        //this.handleSubmit = this.handleSubmit.bind(this);      
+        //this.handleSubmit = this.handleSubmit.bind(this);
+        T.setTexts({
+            first: "Hello, World! My name is *{myName}*!",
+            second:  "How do you do? *{example}*"
+        }, { MDFlavor: 0 });      
     }
 
     // handleInputChange(event) {
@@ -29,6 +34,8 @@ class I18nPage extends React.Component {
         return (
         <div>
            <h3>i18n Proves</h3>
+           <T.p text={{ key: "first", myName: "Gonzalo"}} />
+           <h1>{T.translate("second", { example: "gtormo" })}</h1>
         </div>
             
         );
