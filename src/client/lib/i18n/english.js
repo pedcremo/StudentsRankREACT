61a0fe3babb7b94c035c215c86f0a29d106697b3
gtormo <gonzalotormobas@gmail.com>
2018-02-27 18:58:04 +0100

    some lib dependencies added

---
+++ src/client/lib/i18n/english.js
@@ -0,0 +1,11 @@
+export default
+{
+    headerTitle: "Students Rank",
+    headerSubtitle: "The harder you work, the luckier you get",
+    addStudentTitle: 'Add new Student',
+    addStudentLblFirstName: 'First name',
+    addStudentLblSurnames: 'Surnames',
+    addStudentLblEmail: 'Email',
+    addStudentLblProfileImage: 'Profile Image',
+    addStudentInputSave: 'Save'
+}
